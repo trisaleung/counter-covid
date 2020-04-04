28294e4d9ab32f6e8823334b612a5907ee884823
Trisa Leung <trisaleung1812@gmail.com>
2020-04-03 21:54:18 -0700

    popup fixed

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
+    //Symptoms popup window appears on click, closes when x is clicked
     $(".symptoms").on("click", function(){
-        $(".popupSymptoms, .popupSymptomsContent").addClass("active");
+        $(".popupSymptoms").addClass("active");
+        $(".safety").attr("disabled", true);
     });
 
     $(".popupSymptomsClose, #popupSymptoms").on("click", function(){
-        $(".popupSymptoms, .popupSymptomsContent").removeClass("active");
+        $(".popupSymptoms").removeClass("active");
+        $(".safety").attr("disabled", false);
+    });
+
+    //Safety popup window appears on click, closes when x is clicked
+    $(".safety").on("click", function(){
+        $(".popupSafety").addClass("active");
+        $(".symptoms").attr("disabled", true);
+    });
+
+    $(".popupSafetyClose, #popupSafety").on("click", function(){
+        $(".popupSafety").removeClass("active");
+        $(".symptoms").attr("disabled", false);
     });
 });
