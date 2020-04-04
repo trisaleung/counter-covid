b356ad74e67aa594c0917a16852001007cff7fb1
Trisa Leung <trisaleung1812@gmail.com>
2020-04-03 19:36:29 -0700

    preliminary jquery for popup

---
+++ main.js
@@ -0,0 +1,27 @@
+// //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+// $("#symptoms").on("click", function() {
+//   $("#popup-symptoms").addClass("active");
+// });
+
+// //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+// $(".close, #popup-symptoms").on("click", function() {
+//   $("#popup-symptoms").removeClass("active");
+// });
+
+// $("#safety").on("click", function() {
+//     $("#popup-safety").addClass("active");
+//   });
+
+// $(".close, #popup-safety").on("click", function() {
+//     $("#popup-safety").removeClass("active");
+// });
+
+$(function() {
+    $( "#popup-safety" ).dialog({
+    autoOpen: false,  
+    });
+
+    $("#symptoms").button().click(function () {
+        $("#popup-safety").dialog("open");
+    });
+}
